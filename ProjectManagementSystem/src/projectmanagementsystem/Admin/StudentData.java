8f147a9e33f82c754d60d6582b9b029d3e66ec61
Joshua Tioh <akiharatioh@gmail.com>
2024-04-27 23:19:43 +0800

    segregate parts commit

---
+++ ProjectManagementSystem/src/projectmanagementsystem/Admin/StudentData.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package projectmanagementsystem.Admin;
+
+/**
+ *
+ * @author Tioh
+ */
+public class StudentData {
+        public static String usernameToDelete;
+        public static String passwordToEdit;
+        public static String intakeToEdit;
+}
--- ProjectManagementSystem/src/projectmanagementsystem/Login.java
+++ ProjectManagementSystem/src/projectmanagementsystem/Login.java
@@ -4,6 +4,7 @@
  */
 package projectmanagementsystem;
 
+import projectmanagementsystem.Admin.Admin;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
